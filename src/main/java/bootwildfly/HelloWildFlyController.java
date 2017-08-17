455219d9a5eb204352ebd4637d7b52b2e9efaf9d
krzysztof.kurowski <kuriozalnie@gmail.com>
2017-08-17 13:46:23 +0200

    Trying to connect oracle db through minishift

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -3,12 +3,21 @@ package bootwildfly;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @RestController
 public class HelloWildFlyController {
-
-
     @RequestMapping("hello")
-    public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
+    public String sayHello() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:kredki1", "admki", "admki");
+            connection.close();
+        } catch (Exception e) {
+            return ("Can't connect ;_;");
+        } finally {
+            return ("Connected! :3");
+        }
     }
 }
